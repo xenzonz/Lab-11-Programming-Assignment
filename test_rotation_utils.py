2c11dcabc1fd3ed4e1fead9d290ed185fe00461e
sam cocquyt <samcocquyt04@gmail.com>
2026-04-05 23:49:51 -0400

    even more tests added

--- test_rotation_utils.py
+++ test_rotation_utils.py
@@ -1,4 +1,4 @@
-print("gelloworld")
+#print("gelloworld")
 
 """
 Docstring for test_rotaion_utils.py
@@ -11,6 +11,9 @@ v. 4/5/2026
 
 import pytest
 from rotation_utils import adjust_rotation
+
+
+print(adjust_rotation(360))
 
 def test_adjust_rotation_100():
     assert adjust_rotation(100) == 100
@@ -33,3 +36,24 @@ def test_adjust_rotation_negative820():
 def test_adjust_rotation_string_raises_typerror():
     with pytest.raises(TypeError):
         adjust_rotation("asd")
+
+
+
+def test_adjust_rotation_zero():
+    assert adjust_rotation(0) == 0
+
+
+def test_adjust_rotation_360():
+    assert adjust_rotation(360) == 0
+
+
+def test_adjust_rotation_720():
+    assert adjust_rotation(720) == 0
+
+
+def test_adjust_rotation_float_positive():
+    assert adjust_rotation(100.5) == 100.5
+
+
+def test_adjust_rotation_float_negative():
+    assert adjust_rotation(-45.5) == 314.5
